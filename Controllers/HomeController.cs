490bc3855ce6a81c543a8463df743ba149a17e95
Mikhail Kotov <63475884+mishamur@users.noreply.github.com>
2020-11-21 18:20:07 +0400

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var weeks = new List<WeekDay>();
+
+            var mondey = new WeekDay();
+            mondey.NameDay = "понедельник";
+            mondey.Index = 0;
+            mondey.Lessons = new List<string>();
+            mondey.Lessons.Add("МатАнализ");
+            mondey.Lessons.Add("ООП");
+        
+
+            weeks.Add(mondey);
+
+            ViewBag.Weeks = weeks;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Models/WeekDay.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class WeekDay
+    {
+        public string NameDay { get; set; }
+        public List<string> Lessons { get; set; }
+        public int Index { get; set; }
+
+    }
+}
